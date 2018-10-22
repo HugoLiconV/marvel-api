d6a55fbbc390cac04b1bd903cfed7e251a306ef4
Hugo <hugo_val28@hotmail.com>
2018-10-21 18:47:43 -0600

    add comics grid

--- src/components/Comics/Comic/Comic.js
+++ src/components/Comics/Comic/Comic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Comic.css';
 
 const Comic = () => {
   return (
--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Comics.css'
 import Comic from "./Comic/Comic";
+import './Comics.css'
 
 const Comics = () => {
   return (
-    <div className="Comic">
+    <div className="Comics">
       <h1>Comics</h1>
-      <Comic/>
+      <div className="grid-container">
+        <Comic className="card"/>
+        <Comic className="card"/>
+        <Comic className="card"/>
+        <Comic className="card"/>
+        <Comic className="card"/>
+      </div>
     </div>
   )
 }
