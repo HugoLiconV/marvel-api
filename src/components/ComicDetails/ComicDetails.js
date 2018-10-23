aebfae2691a56a319999d649d736f36fc4a2a026
Hugo <hugo_val28@hotmail.com>
2018-10-23 13:50:13 -0600

    finish comic details

--- src/components/ComicDetails/ComicDetails.js
+++ src/components/ComicDetails/ComicDetails.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import './ComicDetails.css';
+import Characters from '../Characters/Characters';
 
 const ComicDetails = () => (
-  <div className="container">
-  <div className="cell cell-1">
-    <h3>Image</h3>
+  <div>
+    <div className="comic-info-container">
+      <div className="cell cell-1">
+        <h3>Image</h3>
+      </div>
+      <div className="cell cell-2">
+        <h2>Title</h2>
+        <h3>Descripción</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, id 
+          sed. Quas autem dolorem unde ab aut labore possimus sapiente reiciendis 
+          corrupti, numquam distinctio quisquam quia vel eos vero dolorum?
+        </p>
+        <h3>Details</h3>
+        <div className="details-container">
+          Extensión: 210 Páginas
+          Publicado: 2018 10 de septiembre
+        </div>
+        <button>Comprar | $100.00</button>
+        {/* TODO: Agregar icono + */}
+        <button>+ Me interesa</button>
+      </div>
+      {/* <div className="cell cell-3">
+        <h3>Descripción</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, id 
+          sed. Quas autem dolorem unde ab aut labore possimus sapiente reiciendis 
+          corrupti, numquam distinctio quisquam quia vel eos vero dolorum?
+        </p>
+      </div> */}
+      {/* <div className="cell cell-4"> */}
+      {/* </div> */}
+    </div>
+    <Characters/>
   </div>
-  <div className="cell cell-2">
-    <h2>Title</h2>
-
-  </div>
-  <div className="cell cell-3">
-    <h3>Descripción</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, id 
-      sed. Quas autem dolorem unde ab aut labore possimus sapiente reiciendis 
-      corrupti, numquam distinctio quisquam quia vel eos vero dolorum?
-    </p>
-  </div>
-  <div className="cell cell-4"><h3>Details</h3></div>
-</div>
 )
 
 export default ComicDetails;
