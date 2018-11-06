dc4e856d963c5299c41f60fbf91b96c89bd1a3fc
Hugo <hugo_val28@hotmail.com>
2018-11-06 12:43:27 -0700

    add pagination to comics

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './Comics.css'
 import Comic from "./Comic/Comic";
 import './Comics.css'
+import axios from "axios/index";
+import Pagination from "../Pagination/Pagination";
 
 class Comics extends React.Component {
+
+  state = {
+    comics: [],
+    totalComics: 0,
+    limit: 0
+  }
 
   renderComic = comic => {
     return (
@@ -11,12 +19,60 @@ class Comics extends React.Component {
     )
   }
 
+  getComics = (callback, params = {}) => {
+    axios.get('/comics', {
+      params: {
+        ...params
+      }
+    })
+      .then(callback)
+      .catch(error => console.log(error))
+  }
+
+  onPageChanged = data => {
+    const offset = (data.currentPage - 1) * data.pageLimit
+    const handleRequest = res => {
+      const data = res.data;
+      if (data.code === 200) {
+        const comics = data.data.results
+        this.setState({ comics })
+      }
+    };
+    this.getComics(handleRequest, { offset })
+  }
+
+  componentDidMount() {
+    const handleResponse = res => {
+      const data = res.data;
+      if (data.code === 200) {
+        const comic = data.data.results
+        const totalComics = data.data.total
+        const limit = data.data.limit
+        this.setState({
+          comic,
+          totalComics,
+          limit
+        })
+      }
+    }
+    this.getComics(handleResponse)
+  }
+
   render() {
+    if (this.state.totalComics === 0) return null;
     return (
       <div className="Comics">
         <h1>Comics</h1>
+        <div className="u-flex-container-pagination">
+          <Pagination
+            totalRecords={this.state.totalComics}
+            pageLimit={this.state.limit}
+            pageNeighbours={1}
+            onPageChanged={this.onPageChanged}
+          />
+        </div>
         <div className="grid-container">
-          {this.props.comics.map(this.renderComic)}
+          {this.state.comics.map(this.renderComic)}
         </div>
       </div>
     )
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from '../components/Navbar/Navbar';
 import { Switch, Route } from 'react-router-dom'
-import axios from 'axios';
 import Main from '../components/Main/Main'
 import ComicDetails from '../components/ComicDetails/ComicDetails';
 import CharacterDetails from '../components/CharacterDetails/CharacterDetails';
 class App extends Component {
   
   state = {
-    comics: [],
+    // comics: [],
     totalComics: 0,
   }
 
   componentDidMount() {
-    axios.get('/comics')
-    .then(res => {
-      if (res.data.code === 200){
-        const comics = res.data.data.results;
-        this.setState({comics})
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    // axios.get('/comics')
+    // .then(res => {
+    //   if (res.data.code === 200){
+    //     const comics = res.data.data.results;
+    //     this.setState({comics})
+    //   }
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    // })
   }
 
   render() {
@@ -33,11 +32,11 @@ class App extends Component {
          <Switch>
             <Route path="/comics/:id" component={ComicDetails} />
             <Route path="/characters/:id" component={CharacterDetails} />
-            <Route path="/" render={(props)=> <Main {...props} comics={this.state.comics}/>} />
+            <Route path="/" component={Main}/>} />
          </Switch>
       </div>
     );
   }
 }
-
+//render={(props)=> <Main {...props} comics={this.state.comics}
 export default App;
