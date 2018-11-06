809fd68b4ca7ccc302e332925e72d1f1e44a4c47
Hugo <hugo_val28@hotmail.com>
2018-11-05 21:00:29 -0700

    add character card

--- src/components/Characters/Character/Character.js
+++ src/components/Characters/Character/Character.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import './Character.css'
+const getImageUrl = (url, extension) => `${url}/portrait_xlarge.${extension}`
 
-const Character = () => (
-  <h2>Character</h2>
-)
+const Character = (props) => {
+  const character = props.character
+
+  const goToDetails = event => {
+    event.preventDefault();
+    props.history.push(`/characters/${character.id}`)
+  }
+
+  return (
+    <div className="Character u-card" onClick={goToDetails}>
+      <img className="image" src={getImageUrl(character.thumbnail.path, character.thumbnail.extension)} alt=""/>
+      <h2>{character.name}</h2>
+    </div>
+  )
+}
 
 export default Character;
