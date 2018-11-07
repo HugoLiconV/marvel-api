6cedc64fa3858c2e1d5fd7e0316afdc409f8f98d
Hugo <hugo_val28@hotmail.com>
2018-11-07 10:28:30 -0700

    add enzyme

--- src/components/Comics/Comic/Comic.test.js
+++ src/components/Comics/Comic/Comic.test.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Comic from './Comic';
-
+import { shallow } from 'enzyme'
 /* SMOKE TEST */
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Comic/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Comic/>, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
+describe('Comic Component', function () {
+  it('should render', function () {
+    shallow(<Comic/>)
+  });
+});
 
 describe('Comic Component', function () {
   it('should pass', function () {
