8cafbb5f1a930228ec45fbcd04f28372e9e0c637
Hugo <hugo_val28@hotmail.com>
2018-11-16 11:07:10 -0700

    add deploy files and fix details

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middleware = [thunk, logger]
+const middleware = [thunk]
 
 const store = createStore(
   rootReducer,
